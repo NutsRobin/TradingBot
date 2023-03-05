8029a474809324ba2aa886f3df0a29f5cf6b598b
Justin <bowersjm116@gmail.com>
2023-03-05 14:48:19 -0500

    some basic functions to begin testing kraken api

--- ApiTest.py
+++ ApiTest.py
@@ -13,4 +13,18 @@ with open('keys', 'r') as k:
 
 api_url = 'https://api.kraken.com'
 
-#def kraken_signature():
+def kraken_signature(url_path, data, sec):
+    postdata = urllib.parse.urlencode(data)
+    encoded = (str(data['nonce']) + postdata).encode()
+    message = url_path.encode() + hashlib.sha256(encoded).digest()
+
+    mac = hmac.new(base64.b64decode(sec), message, hashlib.sha512)
+    sig_digest = base64.b64encode(mac.digest())
+    return sig_digest.decode()
+
+
+def gen_request(url_path, data, api_key, sec):
+    signature = kraken_signature(url_path, data, sec)
+    headers = {'API-KEY' : api_key, 'API-SIGN' : signature}
+    resp = requests.post((api_url+url_path), headers=headers, data=data)
+    return resp
